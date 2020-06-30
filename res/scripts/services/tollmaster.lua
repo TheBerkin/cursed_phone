4d4244489745a00e1f56640e94ee5f2fefd23bd5
Nicholas Fleck <TheBerkin@users.noreply.github.com>
2020-06-30 18:04:31 -0500

    Okay, perhaps not *that* annoying

--- res/scripts/services/tollmaster.lua
+++ res/scripts/services/tollmaster.lua
@@ -6,7 +6,6 @@ S:state(SERVICE_STATE_IDLE, {
     tick = function(self)
         if toll.is_awaiting_deposit() then
             print("Tollmaster: Insert " .. toll.current_call_rate() .. "¢ now!")
-            sound.play_wait("toll/nag_tone", CHAN_BG2, { volume = TONE_VOLUME })
             sound.play("toll/nag_start_call", CHAN_BG1)
             sound.wait_min(CHAN_SOUL4, 10.0)
         elseif toll.is_time_low() then
