fe62f35a51a57d0d458b4c52f71ef300c41d7989
Robin Pederson <TheBerkin@users.noreply.github.com>
2023-02-05 21:07:18 -0600

    Add wind chimes to the Beyond

--- res/scripts/agents/beyond.lua
+++ res/scripts/agents/beyond.lua
@@ -31,6 +31,15 @@ local function do_seagulls()
     end
 end
 
+local function do_chimes()
+    agent.wait(rand_float(0, 5))
+    sound.play_wait("$beyond/chimes_*", CHAN_PHONE8, { volume = rand_float(0.01, 0.025), speed = rand_float(0.9, 1.05), skip = rand_float(0, 4), fadein = 3 })
+    while true do
+        agent.wait(rand_float(5, 20))
+        sound.play_wait("$beyond/chimes_*", CHAN_PHONE8, { volume = rand_float(0.01, 0.025), speed = rand_float(0.9, 1.05), skip = rand_float(0, 4), fadein = 3 })
+    end
+end
+
 module:state(AGENT_STATE_CALL, {
     enter = function(self)
         if self:get_reason() == CALL_REASON_USER_INIT then
@@ -42,9 +51,9 @@ module:state(AGENT_STATE_CALL, {
 
         sound.play("$beyond/amb_waves", CHAN_PHONE3, { volume = rand_float(0.1, 0.25), speed = rand_float(0.9, 1.1), skip = rand_float(0, 15), looping = true })
         sound.play("$beyond/amb_desert", CHAN_PHONE4, { volume = rand_float(0.1, 0.25), speed = rand_float(0.9, 1.1), skip = rand_float(0, 15), looping = true })
-        sound.play("$beyond/amb_crows", CHAN_PHONE5, { volume = rand_float(0.01, 0.04), speed = rand_float(0.9, 1.1), skip = rand_float(0, 15), looping = true })
+        sound.play("$beyond/amb_crows", CHAN_PHONE5, { volume = rand_float(0.02, 0.04), speed = rand_float(0.9, 1.1), skip = rand_float(0, 15), looping = true })
 
-        agent.multi_task(do_lightning, do_seagulls)
+        agent.multi_task(do_lightning, do_seagulls, do_chimes)
     end
 })
 
