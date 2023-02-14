496f03f2a4b936c8ec51c60eef74e05d1b9d4400
Robin Pederson <TheBerkin@users.noreply.github.com>
2023-02-14 17:49:55 -0600

    redgreen: experiment with heart monitor sfx

--- res/scripts/agents/donovet_redgreen.lua
+++ res/scripts/agents/donovet_redgreen.lua
@@ -1,15 +1,36 @@
+--[[ 
+
+Donovet's Challenge: Red Light / Green Light
+
+SOUND CHANNEL LAYOUT: 
+* PHONE01:  VO (Donovet)
+* PHONE02:  VO (Victim)
+* PHONE03:  Victim Footsteps
+* PHONE04:  Monster Proximity SFX
+* PHONE05:  Monster Voice
+* PHONE06:  Heart Monitor
+* PHONE07:  --
+* Phone08:  --
+* BG01:     Soundscape (Loop)
+* BG02:     Soundscape (Moments - Dripping)
+* BG03:     Soundscape (Moments - Rare)
+* BG04:     --
+]]
+
 local module = create_agent("donovet_redgreen", "6661")
 module:require_sound_bank("redgreen")
 module:accept_all_calls() -- TODO: Remove this later!
 
 -- constants
+local TAU = math.pi * 2.0
 local HEARTBEAT_B_THRESHOLD = 65
 local HEARTBEAT_C_THRESHOLD = 110
-local TAU = math.pi * 2.0
 local OUT_VIBRATE = 27
 local HEART_RATE_START = 65
 local HEARTBEAT_WIDTH = 0.03
-local VICTIM_ESCAPE_DISTANCE = 30
+local VICTIM_ESCAPE_DISTANCE = 120
+local VICTIM_SPEED = 1
+local FOOTSTEP_VOLUME = 0.65
 
 local MONSTER_STATE_IDLE = 0
 local MONSTER_STATE_MENACE = 1
@@ -29,8 +50,11 @@ local game = {
     victim_distance = VICTIM_ESCAPE_DISTANCE,
     --- Is victim currently walking?
     walking = false,
+    --- Monster state
     monster = {
+        --- State code of the current state
         state = MONSTER_STATE_IDLE,
+        --- Active time of the current state
         state_time = time_since()
     },
     --- @param self RedGreenGame
@@ -80,8 +104,8 @@ local function select_heartbeat_bank(bpm)
 end
 
 --- @async
-local function task_ambience()
-    sound.play("$redgreen/ambient/amb_dungeon", Channel.PHONE08, { looping = true, skip = rand_float(0, 30), volume = 0.1 })
+local function task_soundscape()
+    sound.play("$redgreen/ambient/amb_dungeon", Channel.BG01, { looping = true, skip = rand_float(0, 30), volume = 0.15 })
 
     agent.multi_task(
     function()
@@ -89,7 +113,7 @@ local function task_ambience()
 
         while true do
             agent.wait(first_moment and rand_float(0, 25) or rand_float(5, 30))
-            sound.play("$redgreen/ambient/moment_drip_*", Channel.PHONE07, { volume = rand_float(0.01, 0.1), speed = rand_float(0.85, 1.1), interrupt = false })
+            sound.play("$redgreen/ambient/moment_drip_*", Channel.BG02, { volume = rand_float(0.01, 0.1), speed = rand_float(0.85, 1.1), interrupt = false })
             first_moment = false
         end
     end,
@@ -97,12 +121,13 @@ local function task_ambience()
         while true do 
             agent.wait(rand_float(5, 20))
             if chance(0.35) then
-                sound.play_wait("$redgreen/ambient/moment_rare_*", Channel.PHONE06, { volume = rand_float(0.005, 0.125), speed = rand_float(0.9, 1.1) })
+                sound.play_wait("$redgreen/ambient/moment_rare_*", Channel.BG03, { volume = rand_float(0.005, 0.125), speed = rand_float(0.9, 1.1) })
             end
         end
     end)
 end
 
+--- @async
 local function task_monster_sounds()
     sound.play("ambient/static", Channel.PHONE04, { looping = true, volume = 0.175 })
     while true do 
@@ -111,32 +136,38 @@ local function task_monster_sounds()
             volume = 0.3,
             fadein = chance(0.5) and 1 or 0,
             speed = rand_float(0.8, 1.25),
-            skip = chance(0.4) and rand_float(0, 0.5) or 0 
+            skip = chance(0.4) and rand_float(0, 0.5) or 0
         })
     end
 end
 
 --- @async
-local function task_update_heartbeat()
+local function task_heartbeat_sounds()
+    agent.wait(1.0)
     while true do
-        if game.heartbeat_enabled then
+        agent.wait_until(function() return game.heartbeat_enabled end)
+        sound.play_wait("$redgreen/vo/computer_ekg_ready", Channel.PHONE06, { volume = 0.2 })
+        while game.heartbeat_enabled do
             gpio.write_pin(OUT_VIBRATE, GPIO_HIGH)
-            sound.play(select_heartbeat_bank(game.heart_rate), Channel.PHONE01, { speed = rand_float(0.9, 1.1) })
+            sound.play("$redgreen/heart_monitor_beep", Channel.PHONE07, { volume = 0.05, interrupt = false })
+            sound.play(select_heartbeat_bank(game.heart_rate), Channel.PHONE06, { speed = rand_float(0.9, 1.1) })
             agent.wait(HEARTBEAT_WIDTH)
             gpio.write_pin(OUT_VIBRATE, GPIO_LOW)
             if game.heartbeat_enabled then
                 agent.wait_dynamic(get_post_heartbeat_wait_time)
             end
-        else
-            agent.yield()
         end
     end
 end
 
 --- @async
 local function task_update_heart_rate()
+    local noise_seed = rand_seed_32()
+
     while true do
-        game.heart_rate = math.sin(engine_time() * TAU * 0.1) * 30 + 95
+        local noise = (perlin_sample(engine_time(), 0, 3, 1, 0.5, 2.0, noise_seed) + 1) * 0.5
+        game.heart_rate = noise * 30 + 95
+        -- module:log("|" .. string.rep("=", noise * 36) .. string.rep(" ", (1 - noise) * 36) .. "| " .. game.heart_rate)
         agent.yield()
     end
 end
@@ -169,25 +200,29 @@ local function task_update_controls()
 end
 
 --- @async
-local function task_update_footsteps()
+local function task_footstep_sounds()
     local function is_victim_stationary() return not game.walking end
 
     while true do
         if game.walking then
             agent.wait_cancel(rand_float(0.25, 0.6), is_victim_stationary)
             while game.walking do 
-                sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.2, 0.3), speed = rand_float(0.9, 1.1), interrupt = true })
+                sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.4, 0.6) * FOOTSTEP_VOLUME, speed = rand_float(0.9, 1.1), interrupt = true })
                 agent.wait_cancel(rand_float(0.8, 0.9), is_victim_stationary)
             end
             agent.wait(rand_float(0.2, 0.35))
-            sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.1, 0.2), speed = rand_float(0.8, 1), interrupt = true })
+            sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.2, 0.4) * FOOTSTEP_VOLUME, speed = rand_float(0.8, 1), interrupt = true })
             if chance(0.5) then 
                 agent.wait(rand_float(0.2, 0.4))
-                sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.1, 0.15), speed = rand_float(0.6, 0.8), interrupt = true })
+                sound.play("$redgreen/footstep_*", Channel.PHONE03, { volume = rand_float(0.2, 0.5) * FOOTSTEP_VOLUME, speed = rand_float(0.6, 0.8), interrupt = true })
             end
         end
         agent.yield()
     end
+end
+
+local function task_update_victim()
+
 end
 
 local function task_intro()
@@ -197,12 +232,12 @@ end
 module:state(AgentState.CALL, {
     enter = function(self)
         agent.multi_task(
-            task_ambience,
+            task_soundscape,
             -- task_intro,
-            task_update_heartbeat,
+            task_heartbeat_sounds,
             task_update_heart_rate,
             task_update_controls,
-            task_update_footsteps,
+            task_footstep_sounds,
             task_monster_sounds
         )
     end,
